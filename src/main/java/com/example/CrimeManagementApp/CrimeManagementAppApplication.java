960da8791b0a48e38c8f03e8b71daed1d70163ac
Joysbrightt <oladoyinoluwatomisin11@gmail.com>
2024-09-27 16:14:12 +0100

    "started crime management app"

---
+++ src/main/java/com/example/CrimeManagementApp/CrimeManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.CrimeManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrimeManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrimeManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CrimeManagementApp/CrimeManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CrimeManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
